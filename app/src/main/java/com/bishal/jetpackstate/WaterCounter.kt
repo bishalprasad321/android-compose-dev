ca12f75017e770329f8a41a8847e4dea5247c6b1
bishalprasad321 <bishalprasad321@gmail.com>
2023-08-28 21:43:14 +0530

    Handled internal state changes via different APIs of compose

--- app/src/main/java/com/bishal/jetpackstate/WaterCounter.kt
+++ app/src/main/java/com/bishal/jetpackstate/WaterCounter.kt
@@ -7,35 +7,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bishal.jetpackstate.ui.WellnessTaskItem
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StateLessCounter(
+    count: Int, 
+    onIncrement: () -> Unit, 
+    modifier: Modifier = Modifier
+) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember {
-            mutableStateOf(0)
-        }
         if (count > 0) {
-            var showTask by remember {
-                mutableStateOf(true)
-            }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask = false }
-                )
-            }
             Text(
                 text = "You've had $count glasses."
             )
         }
         Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp),
+            onClick = onIncrement,
             enabled = count < 10
         ) {
             Text(
@@ -43,4 +34,18 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             )
         }
     }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    StateLessCounter(count = count, onIncrement = { count++ }, modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CounterPreview() {
+    StatefulCounter()
 }
--- app/src/main/java/com/bishal/jetpackstate/WellnessSceeen.kt
+++ app/src/main/java/com/bishal/jetpackstate/WellnessSceeen.kt
@@ -1,9 +1,35 @@
 package com.bishal.jetpackstate
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bishal.jetpackstate.ui.WellnessTasksList
+import com.bishal.jetpackstate.ui.WellnessViewModel
 
+
+@SuppressLint("RememberReturnType")
 @Composable
-fun WellnessScreen(modifier : Modifier = Modifier) {
-    WaterCounter(modifier)
+fun WellnessScreen(
+    modifier : Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column {
+        StatefulCounter(modifier)
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task -> wellnessViewModel.remove(task)}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessScreenPreview() {
+    WellnessScreen()
 }
